d9880034a226b32dc1d3e92433d039b796400494
piyushsingh90 <piyushsingh11@rocketmail.com>
2016-07-03 14:16:16 +0530

    adding menu items

--- public/menu.js
+++ public/menu.js
@@ -8,10 +8,30 @@
 					{ display: 'About Us', href: 'about.html', children: []},
 					{ display: 'Courses', href: '#', children: [
 						{ display: 'Fitness', href: '#', children: [
-						{ display: 'Yoga', href: 'courses.html', children: []},
-						{ display: 'Zumba', href: '#', children: []}
+															{ display: 'Zumba', href: 'courses.html', children: []},
+															{ display: 'Exercise', href: '#', children: []},
+															{ display: 'Aerobics', href: '#', children: []}
 						]},
-						{ display: 'Cooking', href: '#', children: []}
+						{ display: 'Dance', href: '#', children: []},
+
+						{ display: 'Music', href: '#', children: []},
+
+						{ display: 'Cooking', href: '#', children: [
+															{ display: 'Cooking', href: '#', children: []},
+															{ display: 'Baking', href: '#', children: []},
+						]},
+
+						{ display: 'Painting', href: '#', children: []},
+
+						{ display: 'Art & Craft', href: '#', children: []},
+
+						{ display: 'Yoga', href: '#', children: []},
+
+						{ display: 'Photography', href: '#', children: []},
+
+						{ display: 'Foreign Language', href: '#', children: []}
+
+
 						]},
 					{ display: 'Workshops', href: '#', children: []},
 					{ display: 'Gallery', href: '#', children: []},
