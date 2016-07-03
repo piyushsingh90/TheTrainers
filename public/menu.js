ac7b84eed81c7eaf90d9ce3947e29751ad92bd81
piyushsingh90 <piyushsingh11@rocketmail.com>
2016-07-03 23:33:39 +0530

    templating and routing

--- public/menu.js
+++ public/menu.js
@@ -1,41 +1,74 @@
 (function(){
-			angular.module('myApp', ['bootstrapSubmenu']);
+			var module = angular.module('myApp', ['bootstrapSubmenu','ngRoute']);
 
-			angular.module('myApp')
-				.controller('menuController', function($scope){
+			module.config(['$routeProvider',
+			        function($routeProvider) {
+			            $routeProvider.
+			            	when('/', {
+			                    templateUrl: 'indexView.html',
+			                    controller: 'RouteController'
+			                }).
+			                when('/about', {
+			                    templateUrl: 'aboutView.html',
+			                    controller: 'RouteController'
+			                }).
+			                when('/contact', {
+			                    templateUrl: 'contactView.html',
+			                    controller: 'RouteController'
+			                }).
+			                when('/videos', {
+			                    templateUrl: 'videosView.html',
+			                    controller: 'RouteController'
+			                }).
+			                when('/:param', {
+			                    templateUrl: 'coursesView.html',
+			                    controller: 'RouteController'
+			                }).
+			                otherwise({
+			                    redirectTo: '/'
+			                });
+			        }]);
+
+			module.controller("RouteController", function($scope,$routeParams) {
+					$scope.param = $routeParams.param;
+					console.log('param ' + $scope.param);
+    		});
+
+
+			module.controller('menuController', function($scope){
 					$scope.menuItems = [
-					{ display: 'Home', href: 'index.html', children: []},
-					{ display: 'About Us', href: 'about.html', children: []},
+					{ display: 'Home', href: '#/index', children: []},
+					{ display: 'About Us', href: '#/about', children: []},
 					{ display: 'Courses', href: '#', children: [
 						{ display: 'Fitness', href: '#', children: [
-															{ display: 'Zumba', href: 'courses.html', children: []},
-															{ display: 'Exercise', href: '#', children: []},
-															{ display: 'Aerobics', href: '#', children: []}
+															{ display: 'Zumba', href: '#/zumba', children: []},
+															{ display: 'Exercise', href: '#/exercise', children: []},
+															{ display: 'Aerobics', href: '#/aerobics', children: []}
 						]},
-						{ display: 'Dance', href: '#', children: []},
+						{ display: 'Dance', href: '#/dance', children: []},
 
-						{ display: 'Music', href: '#', children: []},
+						{ display: 'Music', href: '#/music', children: []},
 
 						{ display: 'Cooking', href: '#', children: [
-															{ display: 'Cooking', href: '#', children: []},
-															{ display: 'Baking', href: '#', children: []},
+															{ display: 'Cooking', href: '#/cooking', children: []},
+															{ display: 'Baking', href: '#/baking', children: []},
 						]},
 
-						{ display: 'Painting', href: '#', children: []},
+						{ display: 'Painting', href: '#/painting', children: []},
 
-						{ display: 'Art & Craft', href: '#', children: []},
+						{ display: 'Art & Craft', href: '#/artAndCraft', children: []},
 
-						{ display: 'Yoga', href: '#', children: []},
+						{ display: 'Yoga', href: '#/yoga', children: []},
 
-						{ display: 'Photography', href: '#', children: []},
+						{ display: 'Photography', href: '#/photography', children: []},
 
-						{ display: 'Foreign Language', href: '#', children: []}
+						{ display: 'Foreign Language', href: '#/foreignLanguage', children: []}
 
 
 						]},
 					{ display: 'Workshops', href: '#', children: []},
 					{ display: 'Gallery', href: '#', children: []},
-					{ display: 'Contact', href: 'contact.html', children: []}
+					{ display: 'Contact', href: '#/contact', children: []}
 					];
 				});
 		})();
