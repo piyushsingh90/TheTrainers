e6539d4cd3d3a56ddd9a195af7bc1ca9d3833053
piyushsingh90 <piyushsingh11@rocketmail.com>
2016-07-04 23:28:02 +0530

    using the params in the controller - take 1

--- public/menu.js
+++ public/menu.js
@@ -26,17 +26,85 @@
 			                }).
 			                when('/:param', {
 			                    templateUrl: 'coursesView.html',
-			                    controller: 'RouteController'
+			                    controller: 'TrainerController'
 			                }).
 			                otherwise({
 			                    redirectTo: '/'
 			                });
 			        }]);
 
-			module.controller("RouteController", function($scope,$routeParams) {
+			module.controller("RouteController", function($scope,$routeParams,$http) {
+					//nothing to do as of now
+			});
+
+			module.controller("TrainerController", function($scope,$routeParams,$http) {
 					$scope.param = $routeParams.param;
-					console.log('param ' + $scope.param);
-    		});
+					var parameter = $scope.param;
+					console.log('param ' + parameter);
+
+					$scope.trainerData = [
+									    {
+									      "Name": "First Name",
+									      "Fees": "Rs 4000",
+									      "serviceType": "music",
+									      "openToWorkshops": "Yes",
+									      "area": "Madhapur",
+									      "privateOrGroup":"private",
+									      "languages":"hindi,english",
+									      "place":"Yours",
+									      "noOfClasses":"4",
+									      "image":"trainer1.jpg"
+									    },
+									    {
+									      "Name": "Second Name",
+									      "Fees": "Rs 5000",
+									      "serviceType": "music",
+									      "openToWorkshops": "No",
+									      "area": "hitech",
+									      "privateOrGroup":"group",
+									      "languages":"bengali,english",
+									      "place":"Mine",
+									      "noOfClasses":"7",
+									      "image":"trainer2.jpg"
+									    },
+									    {
+									      "Name": "Third Name",
+									      "Fees": "Rs 6000",
+									      "serviceType": "dance",
+									      "openToWorkshops": "Yes",
+									      "area": "Gachibowli",
+									      "privateOrGroup":"private",
+									      "languages":"telugu,english",
+									      "place":"Yours",
+									      "noOfClasses":"0",
+									      "image":"trainer3.jpg"
+									    },
+									    {
+									      "Name": "Fourth Name",
+									      "Fees": "Rs 0",
+									      "serviceType": "Pata Nhi",
+									      "openToWorkshops": "Yes",
+									      "area": "Begupet",
+									      "privateOrGroup":"private",
+									      "languages":"spanish,english",
+									      "place":"Yours",
+									      "noOfClasses":"4",
+									      "image":"trainer4.jpg"
+									    }
+									  ];
+
+					if(parameter !== undefined){
+						console.log(parameter);
+						$http.get("/trainer/"+ parameter)
+						    .then(function(response) {
+						        console.log('success '+ JSON.stringify(response));
+						        $scope.content = response.data;
+						    }, function(response) {
+						        console.log('error '+ response);
+						        $scope.content = "Something went wrong";
+						    });
+					}
+			});
 
 
 			module.controller('menuController', function($scope){
