627849adcffefb0bfee2bce9925881bdc0185dbe
piyushsingh90 <piyushsingh11@rocketmail.com>
2016-07-11 20:59:49 +0530

    adding DB credentials

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 
 //exports.url = 'mongodb://localhost:27017/sample';
-exports.url = 'mongodb://<dbuser>:<dbpassword>@ds017175.mlab.com:17175/testandtrain';
+exports.url = 'mongodb://TECHUSER:TECHUSR@ds017175.mlab.com:17175/testandtrain';
 
 exports.webPort = 9001;
 
