750580f0c074c3a4922edea3c147b8cd7ef7ff10
piyushsingh90 <piyushsingh11@rocketmail.com>
2016-07-11 20:48:51 +0530

    db changes

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,6 @@
 
-exports.url = 'mongodb://localhost:27017/sample';
+//exports.url = 'mongodb://localhost:27017/sample';
+exports.url = 'mongodb://<dbuser>:<dbpassword>@ds017175.mlab.com:17175/testandtrain';
 
 exports.webPort = 9001;
 
